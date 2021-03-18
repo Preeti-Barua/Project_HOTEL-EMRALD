99e08ce3238683f26b3f4de35d1a73d26549d088
lavinakhichi <lavinakhichi@gmail.com>
2021-03-18 22:46:57 +0530

    commiting rooms backend

---
+++ roomsbackend/hotel/dao/Daointerfaceroom.java
@@ -0,0 +1,24 @@
+package hotel.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import hotel.models.Rooms;
+
+@Repository
+public interface Daointerfaceroom extends JpaRepository<Rooms, Integer>{
+	@Query("from Rooms where booking_status= :x" )
+	public List<Rooms> multiselectbyBs(@Param ("x") String booking_status);
+	
+	@Query("from Rooms where cleaning_status= :y")
+	public List<Rooms> selectallbyCs(@Param ("y") String cleaning_status);
+	
+	
+	
+
+}
